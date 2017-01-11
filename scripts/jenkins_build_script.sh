a5b4275a29dd7f427c5d411c6d27db5aa6e2c7ff
Ryan David <ryandavid773@gmail.com>
2017-01-10 21:47:54 -0800

    More tweaks to Jenkins build, switch to monotonic build versioning

--- scripts/jenkins_build_script.sh
+++ scripts/jenkins_build_script.sh
@@ -29,7 +29,6 @@ else
     export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/Qt/5.7/gcc_64/lib/cmake/
 fi
 
-
 echo "Found architecture: $ARCH_STRING"
 echo "Using additional catkin config flags: $CATKIN_CONFIG_FLAGS"
 
@@ -42,22 +41,38 @@ if [ "$OS_TYPE" == "Linux" ]; then
     rm -rf $DEBIAN_SCRATCH
     mkdir -p $DEBIAN_SCRATCH/DEBIAN
 
+    DATESTAMP=`date +"%Y%m%d"`
+    FORMATTED_BUILD_ID=`printf %05d $BUILD_ID`
+    MAINTAINER=`whoami`
+
     # Create a control file to be used with the debian.
     echo "Package: avc-corvette" >> $DEBIAN_SCRATCH/DEBIAN/control
-    echo "Version: $BUILD_ID" >> $DEBIAN_SCRATCH/DEBIAN/control
+    echo "Version: $DATESTAMP.$FORMATTED_BUILD_ID" >> $DEBIAN_SCRATCH/DEBIAN/control
     echo "Section: base" >> $DEBIAN_SCRATCH/DEBIAN/control
     echo "Priority: optional" >> $DEBIAN_SCRATCH/DEBIAN/control
     echo "Architecture: $ARCH_STRING" >> $DEBIAN_SCRATCH/DEBIAN/control
     echo "Depends: " >> $DEBIAN_SCRATCH/DEBIAN/control
-    echo "Maintainer: jenkins" >> $DEBIAN_SCRATCH/DEBIAN/control
-    echo "Description: Sparkfun AVC Entry, Powerwheels Corvette" >> $DEBIAN_SCRATCH/DEBIAN/control
+    echo "Maintainer: $MAINTAINER" >> $DEBIAN_SCRATCH/DEBIAN/control
     echo "Git-Branch: ${GIT_BRANCH#*/}" >> scripts/control
     echo "Git-Commit: $GIT_COMMIT" >> scripts/control
-    echo "" >> scripts/control
+    echo "Description: Sparkfun AVC Entry, Powerwheels Corvette" >> $DEBIAN_SCRATCH/DEBIAN/control
+
+    # Add an postinst script.
+    echo "#!/bin/bash" >> $DEBIAN_SCRATCH/DEBIAN/postinst
+    echo "chmod 777 $INSTALL_TARGET/log" >> $DEBIAN_SCRATCH/DEBIAN/postinst
+    chmod 0755 $DEBIAN_SCRATCH/DEBIAN/postinst
 
     # Copy the install output to the scratch space.
     mkdir -p $DEBIAN_SCRATCH/`dirname $INSTALL_TARGET`
     cp -ar install $DEBIAN_SCRATCH/$INSTALL_TARGET
+
+    # Export the Git commit information to root
+    echo "Branch: $GIT_BRANCH" >> $DEBIAN_SCRATCH/$INSTALL_TARGET/BUILD_INFO
+    echo "Commit: $GIT_COMMIT" >> $DEBIAN_SCRATCH/$INSTALL_TARGET/BUILD_INFO
+    echo "Build Version: $DATESTAMP.$FORMATTED_BUILD_ID" >> $DEBIAN_SCRATCH/$INSTALL_TARGET/BUILD_INFO
+
+    # Make a 'log' directory. The postinst script will make sure it has the correct permissions set.
+    mkdir $DEBIAN_SCRATCH/$INSTALL_TARGET/log
 
     # Name the Debian.
     DEBIAN_NAME="avc-corvette-${GIT_BRANCH#*/}-${GIT_COMMIT:0:7}-$ARCH_STRING.deb"
@@ -73,3 +88,4 @@ if [ "$OS_TYPE" == "Linux" ]; then
             -F "package[package_file]=@$DEBIAN_NAME" -o $CURL_JSON_NAME
     fi
 fi
+
