8994a85f7527132cd474d3a0f90c22ac2128ba06
Ryan David <ryandavid773@gmail.com>
2017-01-17 22:14:13 -0800

    Add macOS packaging to Jenkins build script

--- scripts/jenkins_build_script.sh
+++ scripts/jenkins_build_script.sh
@@ -12,14 +12,17 @@ ARCH=`uname -m`
 OS_TYPE=`uname`
 INSTALL_TARGET=/usr/local/avc
 DEBIAN_SCRATCH=/tmp/avc-corvette
+PKG_SCRATCH=/tmp/avc-corvette
 
-# Only build the required nodes on the Beaglebones.
-if [ "$ARCH" == "armv7l" ]; then
+# BeagleBone Black, Ubuntu 14.04
+if [[ ( "$ARCH" == "armv7l" ) && ( "$OS_TYPE" == "Linux" ) ]]; then
     CATKIN_CONFIG_FLAGS="--whitelist motor_control"
     ARCH_STRING="armhf"
     # Packagecloud Distro ID 20 = Ubuntu 14.04.
     PC_DISTRO=20
-else
+
+# x86_64 Desktop, Ubuntu 16.04
+elif [[ ( "$ARCH" == "x86_64" ) && ( "$OS_TYPE" == "Linux" ) ]]; then
     CATKIN_CONFIG_FLAGS=""
     ARCH_STRING="amd64"
     # Packagecloud Distro ID 165 = Ubuntu 16.04.
@@ -27,9 +30,14 @@ else
     
     # Explicitly add Qt5 libs the CMAKE_PREFIX_PATH
     export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/Qt/5.7/gcc_64/lib/cmake/
+
+# x86_64 Desktop, macOS Sierra
+elif [[ ( "$ARCH" == "x86_64" ) && ( "$OS_TYPE" == "Darwin" ) ]]; then
+    CATKIN_CONFIG_FLAGS=""
+    ARCH_STRING="x86_64"
 fi
 
-echo "Found architecture: $ARCH_STRING"
+echo "Found architecture: $ARCH_STRING, $OS_TYPE"
 echo "Using additional catkin config flags: $CATKIN_CONFIG_FLAGS"
 
 catkin config --install $CATKIN_CONFIG_FLAGS
@@ -87,5 +95,33 @@ if [ "$OS_TYPE" == "Linux" ]; then
             -F "package[distro_version_id]=$PC_DISTRO"     \
             -F "package[package_file]=@$DEBIAN_NAME" -o $CURL_JSON_NAME
     fi
-fi
 
+# Otherwise, create a macOS package installer.
+elif [ "$OS_TYPE" == "Darwin" ]; then
+    # Make sure the scratch space is empty, and re-create the desired structure.
+    rm -rf $PKG_SCRATCH
+    mkdir -p $PKG_SCRATCH/`dirname $INSTALL_TARGET`
+    
+    # Copy the newly built project into the scratch space.
+    cp -r install $PKG_SCRATCH/$INSTALL_TARGET
+    
+    # Make a 'log' directory. The postinst script will make sure it has the correct permissions set.
+    mkdir -p $PKG_SCRATCH/$INSTALL_TARGET/log
+
+    # Make a post-install script.
+    mkdir -p $PKG_SCRATCH/scripts
+    echo "#!/bin/bash" >> $PKG_SCRATCH/scripts/postinstall
+    echo "chmod 777 $INSTALL_TARGET/log" >> $PKG_SCRATCH/scripts/postinstall
+    chmod 0755 $PKG_SCRATCH/scripts/postinstall
+    
+    DATESTAMP=`date +"%Y%m%d"`
+    FORMATTED_BUILD_ID=`printf %05d $BUILD_ID`
+    PKG_NAME="avc-corvette-${GIT_BRANCH#*/}-${GIT_COMMIT:0:7}-$ARCH_STRING.pkg"
+
+    # Export the Git commit information to root
+    echo "Branch: $GIT_BRANCH" >> $PKG_SCRATCH/$INSTALL_TARGET/BUILD_INFO
+    echo "Commit: $GIT_COMMIT" >> $PKG_SCRATCH/$INSTALL_TARGET/BUILD_INFO
+    echo "Build Version: $DATESTAMP.$FORMATTED_BUILD_ID" >> $PKG_SCRATCH/$INSTALL_TARGET/BUILD_INFO
+    
+    pkgbuild --root $PKG_SCRATCH --scripts $PKG_SCRATCH/scripts --identifier com.avc-corvette.${GIT_BRANCH#*/} --version 0.$FORMATTED_BUILD_ID $PKG_NAME
+fi
