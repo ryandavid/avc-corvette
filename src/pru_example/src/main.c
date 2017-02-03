c298e9c9fd427a294395f8fb5378ae51f86af8cb
Ryan David <ryandavid773@gmail.com>
2017-02-03 07:10:56 +0000

    Working PRU GPIO example in C

---
+++ src/pru_example/src/main.c
@@ -0,0 +1,28 @@
+/***************************************************************************************
+ * MAIN.C
+ *
+ * Description: main source file for PRU development
+ *
+ * Rafael de Souza
+ * (C) 2015 Texas Instruments, Inc
+ * 
+ * Built with Code Composer Studio v6
+ **************************************************************************************/
+
+#include <stdint.h>
+#include <pru_example/resource_table_empty.h>
+
+volatile register unsigned int __R31, __R30;
+
+int main(void) {
+    unsigned int loops;
+    for (loops = 0; loops < 10; loops++) {
+        __R30 = __R30 | (1 << 15); // Turn on the LED
+        __R30 = __R30 & ~(1 << 15); // Turn off the LED
+    }
+
+    // Send interrupt to host and halt
+    __R31 = 32 | 3;
+    __halt();
+    return 0;
+}
